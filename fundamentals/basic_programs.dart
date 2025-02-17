ff20e3948b2f7b66f7a6373908a569c9e253d4e1
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-02-17 05:07:31 +0430

    Dart fundamentals completed

--- fundamentals/basic_programs.dart
+++ fundamentals/basic_programs.dart
@@ -1,16 +1,59 @@
 import 'dart:io';
 void main (){
 
-String result = evenOddChecker();
-print (result);
-  
+print (evenOddChecker(10));
+
+int factoialNum = 5;
+print('Factorial of $factoialNum is: ${factorial(factoialNum)}');
+
+List<int> numbers = [1,2,3,4,5,6,7,8,9,10];
+print('The sunm of list\'s elements is: ${sumOfListNumbers(numbers)}');
+
+int radius = 12;
+print('The circle radius: $radius, The circle Area: ${circleArea(radius)}');
+
+//Write a program that takes user input and prints whether it is a positive, negative, or zero.
+stdout.write('Please enter a number: ');
+String? input = stdin.readLineSync();
+int number = int.parse(input!);
+
+  if(number > 0){
+    print('The inputted number is positive.');
+  }else if(number < 0){
+    print('The inputted number is negative.'); 
+  }else{
+    print('The input is ZERO!');
+  }
 }
 
-String evenOddChecker(){
-  stdout.write('Please enter a number: ');
-  String? scanner = stdin.readLineSync();
-  int input = int.parse(scanner!);
+//Write a function to check if a number is even or odd.
+String evenOddChecker(int input){
   String result;
-  input % 2 == 0 ? result = 'The input is even' : result =  'The input is odd'; 
+  input % 2 == 0 ? result = '$input is even' : result =  '$input is odd'; 
   return result;
 }
+
+//Write a function that calculates the factorial of a given number.
+int factorial(int number){
+  if(number == 0 || number ==1){
+    return 1;
+  }else{
+    return number * factorial(number - 1);
+  }
+}
+
+//Write a function to find the sum of all numbers in an array.
+int sumOfListNumbers(List<int> input){
+  int sum = 0;
+for(int i = 0; i< input.length; i++){
+  sum += input[i];
+}
+return sum;
+}
+
+//What are arrow functions in JavaScript, and how are they different from regular functions?
+//Answer: Arrow functions in dart are used when the function executes only one expression
+double circleArea(int radius) => 3.14 * (radius * radius);
+
+
+
