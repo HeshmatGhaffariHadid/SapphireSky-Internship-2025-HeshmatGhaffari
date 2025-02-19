b8a20082225ae667a276a9e87789519233329295
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-02-20 04:08:16 +0430

    Code refined

--- fundamentals/basic_programs.dart
+++ fundamentals/basic_programs.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 void main (){
 
+checkNumberCondition();
+
 print (evenOddChecker(10));
 
-int factoialNum = 5;
-print('Factorial of $factoialNum is: ${factorial(factoialNum)}');
+int factorialNum = 5;
+print('Factorial of $factorialNum is: ${factorial(factorialNum)}');
 
 List<int> numbers = [1,2,3,4,5,6,7,8,9,10];
 print('The sunm of list\'s elements is: ${sumOfListNumbers(numbers)}');
@@ -12,8 +14,11 @@ print('The sunm of list\'s elements is: ${sumOfListNumbers(numbers)}');
 int radius = 12;
 print('The circle radius: $radius, The circle Area: ${circleArea(radius)}');
 
+}
+
 //Write a program that takes user input and prints whether it is a positive, negative, or zero.
-stdout.write('Please enter a number: ');
+void checkNumberCondition(){
+stdout.write('Please enter a number to check if it is positive negative or zero: ');
 String? input = stdin.readLineSync();
 int number = int.parse(input!);
 
@@ -27,10 +32,8 @@ int number = int.parse(input!);
 }
 
 //Write a function to check if a number is even or odd.
-String evenOddChecker(int input){
-  String result;
-  input % 2 == 0 ? result = '$input is even' : result =  '$input is odd'; 
-  return result;
+String evenOddChecker(int input){  
+  return input % 2 == 0 ? '$input is even' : '$input is odd'; 
 }
 
 //Write a function that calculates the factorial of a given number.
