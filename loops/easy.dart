4ff9ccf10956d92c60dfc01d53580dbbff4e07da
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-02-20 04:28:06 +0430

    sum of natural numbers method added

--- loops/easy.dart
+++ loops/easy.dart
@@ -1,9 +1,16 @@
 void main (){
-  printUpTo100();
+  // printUpTo100();
+  print(sumNuturalNumber(3));
    
 }
+//print 1 to 100
 void printUpTo100(){
   for(int i = 1; i <= 100; i++){
     print(i);
   }
+}
+//Sum of first N nutural numbers
+double sumNuturalNumber(int n){
+  double sum = n * (n + 1) / 2;
+  return sum;
 }
