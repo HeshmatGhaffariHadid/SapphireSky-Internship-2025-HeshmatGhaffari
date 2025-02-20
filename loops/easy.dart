f93f5eeb90b76b1d196b1fedf5a90ed019ba1608
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-02-20 04:42:35 +0430

    multiplication table added

--- loops/easy.dart
+++ loops/easy.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 void main (){
   // printUpTo100();
-  print(sumNuturalNumber(3));
+  // print(sumNuturalNumber(3));
+  multiplicationTable();
    
 }
 //print 1 to 100
@@ -13,4 +15,13 @@ void printUpTo100(){
 double sumNuturalNumber(int n){
   double sum = n * (n + 1) / 2;
   return sum;
+}
+//Multiuplication table
+void multiplicationTable(){
+  stdout.write('Please enter a number to check it\'s multiplication table: ');
+  String input = stdin.readLineSync()!;
+  int number = int.parse(input);
+    for(int i = 1; i <= 10; i++){
+      print('$i * $number: ${number * i}');
+    }
 }
