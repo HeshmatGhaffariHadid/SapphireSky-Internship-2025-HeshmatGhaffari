35f304246692aae9f9e769b85b994123730eeee7
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-02-20 11:22:22 +0430

    Palindrome checker method added

--- loops/medium.dart
+++ loops/medium.dart
@@ -1,9 +1,11 @@
 void main (){
     // checkPrimeNumber(21);
     // findFactorial(5);
-    
+
     // List<int> numbers = [1,2,3,45,6,7,3,0,-12,22,81];
     // findLargestAndSmallest(numbers);
+    // print(fibonacciSequence(4));
+    // checkPalindrome(1221);
 
 }
 //Check for prime number
@@ -42,4 +44,36 @@ void findLargestAndSmallest(List<int> list){
         }
     }
   print('The largest is $largest, and the smallest is $smallest');
+}
+
+//Fibonacci Sequence F(n) = F(n-1) + F(n-2)
+int fibonacciSequence(int n){
+    int? result;
+    if(n == 0){
+        result = 0;
+    }else if(n == 1){
+        result = 1;
+    }else if(n > 1){
+        for(int i = 2; i<= n; i++){
+            result = (i - 1) + (i - 2);
+        }
+        //I don't have a true minding the fibonacci sequence
+    }
+    return result!;
+}
+
+//Palindrome checker
+void checkPalindrome(int number) {
+    int inputNumber = number;
+    int reverse = 0;
+    while (number > 0){
+        int digit = number % 10;
+        reverse = reverse * 10 + digit;
+        number ~/= 10; 
+    }
+    if(reverse == inputNumber){
+        print('$inputNumber is palindrome');
+    }else{
+        print('$inputNumber is not palindrome!');
+    }
 }
