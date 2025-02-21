7d2cfaf29837397f08a7a151d980455930c7b195
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-02-21 12:13:48 +0430

    count vowels function added

--- functions/easy.dart
+++ functions/easy.dart
@@ -1,32 +1,51 @@
 import 'dart:io';
 
 void main() {
-    // celsiusToFahrenheit(30.7);
-    // maxOfThree(12, 18, 2129);
-    // print(isEven(12));
-    
+  // celsiusToFahrenheit(30.7);
+  // maxOfThree(12, 18, 2129);
+  // print(isEven(12));
+ print(countVowels('Hello world'));
 }
 
 //Convert Celsius to Fahrenheit
 void celsiusToFahrenheit(double celsius) {
-    double fahrenheit;
-    fahrenheit = (celsius * 9 / 5) + 32;
-    print('$celsius celsius is equal to $fahrenheit fahreheit');
+  double fahrenheit;
+  fahrenheit = (celsius * 9 / 5) + 32;
+  print('$celsius celsius is equal to $fahrenheit fahreheit');
 }
+
 //Find the Maximum of Three Numbers
-void maxOfThree (int a, int b, int c) {
-    if(a > b && a > c) {
-        print('The maximum number is: $a');
-    }else if(b > a && b > c){
-        print('The maximum number is: $b');
-    }else{
-        print('The maximum number is: $c');
-    }
+void maxOfThree(int a, int b, int c) {
+  if (a > b && a > c) {
+    print('The maximum number is: $a');
+  } else if (b > a && b > c) {
+    print('The maximum number is: $b');
+  } else {
+    print('The maximum number is: $c');
+  }
 }
+
 //Check Even or Odd
 bool isEven(int number) {
-    if(number % 2 == 0){
-        return true;
+  if (number % 2 == 0) {
+    return true;
+  }
+  return false;
+}
+
+//Count vowels in a String
+int countVowels(String input) {
+  //we can use built in function of countVowel()
+  List vowels = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'];
+  String check;
+  int counter = 0;
+  for (int i = 0; i < vowels.length; i++) {
+    check = vowels[i];
+    for (int j = 0; j < input.length; j++) {
+      if (check == input[j]) {
+        counter++;
+      }
     }
-    return false;
+  }
+  return counter;
 }
