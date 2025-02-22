6c926ea0eb6b8bd8b5aa15eac56a876bebede8d5
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-02-23 00:11:58 +0430

    finding nth fibonacci number method added

--- functions/medium.dart
+++ functions/medium.dart
@@ -1,6 +1,7 @@
 void main() {
   // print(isPrime(53));
-  print(factorial(3));
+  // print(factorial(3));
+  print(fibonacci(8));
 }
 //Check if a Number is Prime
 bool isPrime(int number) {
@@ -18,4 +19,17 @@ int factorial(int number) {
   }else{
     return number * factorial(number -1);
   }
+}
+
+//Find the GCD of Two Numbers
+// int gcd(int a, int b) {
+
+// }
+
+//Find the nth Fibonacci Number
+int fibonacci(int number) {
+  if(number == 0) return 0;
+  if(number == 1) return 1;
+  return fibonacci(number -1) + fibonacci(number -2);
+
 }
