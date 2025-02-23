a992efb317c4c57f4caee93136440f97f7e1bf51
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-02-24 00:17:24 +0430

    bubble sort algorithm added

--- functions/hard.dart
+++ functions/hard.dart
@@ -4,13 +4,13 @@ import 'easy.dart';
 
 void main() {
 // pascalTriangle(4);
-// List<int> list = [1,2,3,4,5,6,7,8,9,10,12,17];
+  List<int> list = [12, 32, 1, 3, 6, 18, 5, 8, 11, 0, 15, 8];
 // print('The index of targeted number in the array is: ${binarySearch(list,12)}');
-decimalToBinary(4);
-
+// decimalToBinary(4);
+  bubbleSort(list);
 }
 
-//Generate Pascal’s Triangle 
+//Generate Pascal’s Triangle
 void pascalTriangle(int rows) {
   // int element;
 
@@ -19,23 +19,22 @@ void pascalTriangle(int rows) {
   //     stdout.write(' ');
   //   }
   //   for (int k = 0; k < (i * 2) - 1; k++) {
-      
+
   //     element = (factorial(rows) / (factorial(k) * factorial(rows - k))).round();
-  //     stdout.write(element);   
+  //     stdout.write(element);
   //   }
   //   print('');
   // }
-
 }
 //Binary Search Algorithm
 int binarySearch(List<int> list, int target) {
-  int end = list.length -1;
+  int end = list.length - 1;
   int middle = (list.length / 2).round();
 
-  while(list[middle] != target) {
-    if(list[middle] > target){
-        middle ~/= 2;
-    }else{
+  while (list[middle] != target) {
+    if (list[middle] > target) {
+      middle ~/= 2;
+    } else {
       middle = ((middle + end) / 2).round();
     }
   }
@@ -52,4 +51,20 @@ void decimalToBinary(int num) {
     num ~/= 2;
   }
   print('The binary of $number is: ${reverseString(result)}');
+}
+
+//Sorting an Array Using Bubble Sort
+void bubbleSort(List<int> arr) {
+  int temp;
+
+  for (int i = arr.length - 1; i >= 1; i--) {
+    for (int j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  print(arr);
 }
