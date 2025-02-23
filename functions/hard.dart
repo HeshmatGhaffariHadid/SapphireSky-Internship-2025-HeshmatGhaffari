7b820ef853d2c25cb9bdbb625655f7ed3b872e40
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-02-24 03:23:27 +0430

    finding unique elements in array method added

--- functions/hard.dart
+++ functions/hard.dart
@@ -1,13 +1,15 @@
+import 'dart:ffi';
 import 'dart:io';
 import '../fundamentals/basic_programs.dart';
 import 'easy.dart';
 
 void main() {
 // pascalTriangle(4);
-  List<int> list = [12, 32, 1, 3, 6, 18, 5, 8, 11, 0, 15, 8];
-// print('The index of targeted number in the array is: ${binarySearch(list,12)}');
+  List<int> arr = [1, 1, 2, 3, 4, 4, 5,8];
+// print('The index of targeted number in the array is: ${binarySearch(arr,12)}');
 // decimalToBinary(4);
-  bubbleSort(list);
+// bubbleSort(arr);
+  findUnique(arr);
 }
 
 //Generate Pascal’s Triangle
@@ -26,6 +28,7 @@ void pascalTriangle(int rows) {
   //   print('');
   // }
 }
+
 //Binary Search Algorithm
 int binarySearch(List<int> list, int target) {
   int end = list.length - 1;
@@ -67,4 +70,24 @@ void bubbleSort(List<int> arr) {
     }
   }
   print(arr);
+}
+
+//Find Unique Elements in an Array
+void findUnique(List<int> arr) {
+  List<int> uniqueArr = [];
+  
+  for (int i = 0; i < arr.length; i++) {
+    bool isDupplicate = false;
+    
+    for(int j = 0; j < arr.length; j++) {
+      if(i != j && arr[i] == arr[j]) {
+        isDupplicate = true;
+        break;
+      }
+    }
+    if(!isDupplicate) {
+      uniqueArr.add(arr[i]);
+    }
+  }
+  print(uniqueArr);
 }
