5cbb3151095bbe5c0e2fb01149d846e2832b70ce
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-02-24 11:27:21 +0430

    determine season method added

--- switch-case/medium.dart
+++ switch-case/medium.dart
@@ -42,3 +42,26 @@ void useATMsystem(int input) {
     exit(0);
   }
 }
+//Determine Season by Month
+String determineSeason (int numberOfMonth) {
+  switch(numberOfMonth) {
+    case 12:
+    case 1:
+    case 2:
+    return 'Winter';
+    case 3:
+    case 4:
+    case 5:
+    return 'Spring';
+    case 6:
+    case 7:
+    case 8:
+    return 'Summer';
+    case 9:
+    case 10:
+    case 11:
+    return 'Fall';
+    default: 
+    return 'Invalid month number';
+  }
+}
