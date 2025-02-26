ccff4b1225e65d99ce90fb73f2d777c50e709ac9
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-02-26 07:39:47 +0430

    finding prime numbers up to 100 using nested loops added

---
+++ bonus-challenges/nested_loops.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import '../switch-case/hard.dart';
+
+void main() {
+  findPrime();
+}
+// Print Prime numbers from 1 - 100
+void findPrime() {
+  List primeNums = [];
+  bool isPrime;
+  for (int i = 1; i < 100; i+=2) {
+    isPrime = false;
+    for (int j = 2; j < i; j++) {
+      if (i % j == 0) {
+        isPrime = true;
+      }
+    }
+    if(!isPrime) {
+      primeNums.add(i);
+    }
+  }
+  print(primeNums.join(', '));
+}
