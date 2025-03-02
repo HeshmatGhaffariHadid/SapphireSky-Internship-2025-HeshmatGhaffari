bf6750eda1dc6adaced6beba12f93425ad6812c9
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-03 03:42:36 +0430

    Occurrence counter method added

--- Arrays/simple.dart
+++ Arrays/simple.dart
@@ -1,8 +1,9 @@
 void main() {
-  List<int> list = [1, 2, 3, 4, 5, 6, 7, 10];
-  print(findLargest(list));
-  print(sumOfElements(list));
-  print(checkElement(list, 4));
+  List<int> list = [1, 2, 3, 4, 5, 6, 2, 3, 4, 2, 7, 10];
+  // print(findLargest(list));
+  // print(sumOfElements(list));
+  // print(checkElement(list, 4));
+  countOccurrence(list, 2);
 }
 
 //Write a function to find the largest element in an array.
@@ -17,7 +18,7 @@ int findLargest(List<int> list) {
 //Write a function to find the sum of all elements in an array.
 int sumOfElements(List<int> list) {
   int sum = 0;
-  for(int i = 0; i< list.length; i++) {
+  for (int i = 0; i < list.length; i++) {
     sum += list[i];
   }
   return sum;
@@ -26,8 +27,18 @@ int sumOfElements(List<int> list) {
 //How do you check if an array contains a specific element?
 bool checkElement(List<int> list, int element) {
   bool check = false;
-  for(int i = 0; i< list.length; i++) {
-    if(list[i] == element) check = true;
+  for (int i = 0; i < list.length; i++) {
+    if (list[i] == element) check = true;
   }
   return check;
 }
+
+//Write a function to count the occurrences of a specific element in an array.
+void countOccurrence(List<int> list, int element) {
+  int counter = 0;
+  for (int i = 0; i < list.length; i++) {
+    if (list[i] == element) counter++;
+  }
+  print('The occurrence of $element is: $counter');
+}
+
