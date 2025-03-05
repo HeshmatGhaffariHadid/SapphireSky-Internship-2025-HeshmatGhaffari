5783b7a72e6cd17e1fe2d7558dbcf759c56352a8
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-06 00:47:07 +0430

    Method to find maximum sum of subarray

--- Arrays/hard.dart
+++ Arrays/hard.dart
@@ -1,10 +1,9 @@
-import 'dart:developer';
-
 void main() {
-  List<int> list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15];
-  List<int> list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15];
-  // print('The missing number is: ${findMissingNum(list)}');
-  print(checkArraysEqualance(list1, list2));
+  List<int> list = [1, -2, 3, -1, 5, 2, 3];
+  // List<int> list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15];
+  // // print('The missing number is: ${findMissingNum(list)}');
+  // print(checkArraysEqualance(list1, list2));
+  print('Maximum sum subarray is: ${findMaxSubarraySum(list)}');
 }
 
 // How do you remove duplicates from an array efficiently?
@@ -44,3 +43,27 @@ bool checkArraysEqualance(List<int> list1, List<int> list2) {
   return true;
 }
 
+// Implement a function to find the subarray with the maximum sum (Kadane’s Algorithm).
+List<int> findMaxSubarraySum(List<int> list) {
+  int maxEnding = list[0];
+  int maxSoFar = list[0];
+  int start = 0;
+  int end = 0;
+  int temp = 0;
+
+  for (int i = 1; i < list.length; i++) {
+    if (list[i] > maxEnding + list[i]) {
+      maxEnding = list[i];
+      temp = i;
+    } else {
+      maxEnding += list[i];
+    }
+
+    if (maxEnding > maxSoFar) {
+      maxSoFar = maxEnding;
+      start = temp;
+      end = i;
+    }
+  }
+  return list.sublist(start, end +1);
+}
