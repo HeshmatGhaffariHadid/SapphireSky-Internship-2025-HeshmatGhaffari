f12aaf1afac48948fbffcc18c1046e1738bcafc7
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-05 12:31:40 +0430

    Method of checking arrsys equalance added

--- Arrays/hard.dart
+++ Arrays/hard.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 void main() {
-  List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15];
-  print('The missing number is: ${findMissingNum(list)}');
+  List<int> list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15];
+  List<int> list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15];
+  // print('The missing number is: ${findMissingNum(list)}');
+  print(checkArraysEqualance(list1, list2));
 }
 
 // How do you remove duplicates from an array efficiently?
@@ -29,5 +33,14 @@ int findMissingNum(List<int> list) {
     }
   }
   return missedNum;
+}
+
+// How do you check if two arrays are equal?
+bool checkArraysEqualance(List<int> list1, List<int> list2) {
+  if (list1.length != list2.length) return false;
+  for (int i = 0; i < list1.length; i++) {
+    if (list1[i] != list2[i]) return false;
+  }
+  return true;
 }
 
