c53211abce8af0fd6819bbc7e56eadd0974e977e
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-07 04:55:18 +0430

    method to check anagram strings added

--- medium.dart
+++ medium.dart
@@ -1,6 +1,7 @@
+import 'dart:ffi';
+
 void main() {
-  print(
-      'The first non-repeating letter of your input is: ${findNonRepeatingChar('Hello')}');
+ print(checkAnagramStrings('racee', 'carew'));
 }
 
 //How can you find the first non-repeating character in a string?
@@ -13,3 +14,14 @@ String findNonRepeatingChar(String input) {
   }
   return 'No unique charecter exists!';
 }
+
+// Write a function to check if two strings are anagrams.        race = care
+ bool checkAnagramStrings (String input1, String input2) {
+  if (input1.length != input2.length) return false;
+  List<String> list1 = input1.split('');
+  List<String> list2 = input2.split('');
+  list1.sort();
+  list2.sort();
+  if(list1.join() == list2.join()) return true;
+  return false;
+ }
