429996af70962959a721021b1a68d1af95361f7c
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-10 05:22:15 +0430

    method to count char's occurrence added

--- hard.dart
+++ hard.dart
@@ -1,5 +1,7 @@
-void main() {
+import 'dart:io';
 
+void main() {
+  countCharOccurrence('inuputpi');
 }
 
 // Implement a function to find the longest palindrome substring in a string.
@@ -13,7 +15,7 @@ void main() {
 //       left--;
 //       right++;
 //     }
-    
+
 //     int length = right - left - 1;
 //     if (length > maxLength) {
 //       start = left + 1;
@@ -22,32 +24,46 @@ void main() {
 //   }
 
 //   for (int i = 0; i < s.length; i++) {
-//     expandAroundCenter(i, i);    
-//     expandAroundCenter(i, i + 1); 
+//     expandAroundCenter(i, i);
+//     expandAroundCenter(i, i + 1);
 //   }
 
 //   return s.substring(start, start + maxLength);
 // }
 
 // Write a function to find all permutations of a given string.
-void permuteString(String str, int left, int right) {
+void permuteString(String input, int left, int right) {
   if (left == right) {
-    print(str); // Print the permutation
+    print(input);
     return;
   }
 
   for (int i = left; i <= right; i++) {
-    str = swap(str, left, i); 
-    permuteString(str, left + 1, right);  
-    str = swap(str, left, i); 
+    input = swap(input, left, i);
+    permuteString(input, left + 1, right);
+    input = swap(input, left, i);
   }
 }
 
-String swap(String str, int i, int j) {
-  List<String> charArray = str.split('');
+String swap(String input, int i, int j) {
+  List<String> charArray = input.split('');
   String temp = charArray[i];
   charArray[i] = charArray[j];
   charArray[j] = temp;
   return charArray.join('');
 }
 
+// Implement a function to count the number of occurrences of each character in a string.
+void countCharOccurrence(String input) {
+  List<String> results = [];
+  for (int i = 0; i < input.length; i++) {
+    int counter = 0;
+    for (int j = 0; j < input.length; j++) {
+      if (input[i] == input[j]) {
+        counter++;
+      }
+    }
+    results.add('${input[i]} = $counter ');
+  }
+  print(results.toSet().join('\n'));
+}
