ac0e4a022675c97d40787c5bff0090575285cd31
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-12 16:39:58 +0430

    Linked List Please check the linked list part question

---
+++ simple/simple.dart
@@ -0,0 +1,5 @@
+/* 1- What is a linked list, and how does it differ from an array?
+Linked list is a linear data structure where elements are connected togather using pointers. In linked list each node 
+contains data and a pointer to the next node in the list. */
+
+// Implement a function to insert a node at the beginning of a linked list.
