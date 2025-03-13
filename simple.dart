8f699d143e9de36a79981dce96618f3acb238536
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-13 15:47:56 +0430

    method to delete the first node added

--- simple.dart
+++ simple.dart
@@ -1,8 +1,10 @@
+void main() {}
+
 /* 1- What is a linked list, and how does it differ from an array?
 Linked list is a linear data structure where elements are connected togather using pointers. In linked list each node 
 contains data and a pointer to the next node in the list. */
 
-// Implement a function to insert a node at the beginning of a linked list.
+
 class Node {
   int data;
   Node? next;
@@ -11,9 +13,17 @@ class Node {
 class LinkedList{
   Node? firstNode;
 
+// Implement a function to insert a node at the beginning of a linked list.
   void addFront(int data) {
     Node newNode = Node(data);
     newNode.next = firstNode;
     firstNode = newNode;
+  }
+  // How do you delete a node from a linked list?
+  void deleteNode() {
+    if(firstNode == null) return;
+    Node? temp = firstNode!.next;
+    firstNode!.next = null;
+    firstNode = temp;
   }
 }
