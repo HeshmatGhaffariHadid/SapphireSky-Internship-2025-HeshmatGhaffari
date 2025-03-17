1949baf147054644006665970b17f59ba3f0d851
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-17 14:56:38 +0430

    mergeSortedLists method added

--- simple.dart
+++ simple.dart
@@ -104,4 +104,26 @@ class LinkedList {
     }
     return false;
   }
+  // Write a function to merge two sorted linked lists.
+  Node? mergeSortedLists(Node? list1, Node? list2) {
+  Node dummy = Node(0);
+  Node? current = dummy;
+
+  while (list1 != null && list2 != null) {
+    if (list1.data <= list2.data) {
+      current!.next = list1;
+      list1 = list1.next;
+    } else {
+      current!.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next!;
+  }
+  if (list1 != null) {
+    current!.next = list1;
+  } else if (list2 != null) {
+    current!.next = list2;
+  }
+  return dummy.next;
+}
 }
