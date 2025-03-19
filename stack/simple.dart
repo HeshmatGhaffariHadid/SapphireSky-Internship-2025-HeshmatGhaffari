a4e894036dc1d6831513dd4f6cfb83f205964390
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-19 23:03:46 +0430

    stack defined

---
+++ stack/simple.dart
@@ -0,0 +1,4 @@
+// 1- What is a stack, and how does it follow the LIFO principle?
+// Stack is a data structure that stores object in LIFO (Last In First Out) algorithm, it means like a verticle tower
+// that we have access to the top object of the stack.
+
