d54390704aefb27dd5fca811df1c7f065cb8adf6
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-19 23:04:19 +0430

    stack using list implemented

--- stack/simple.dart
+++ stack/simple.dart
@@ -2,3 +2,29 @@
 // Stack is a data structure that stores object in LIFO (Last In First Out) algorithm, it means like a verticle tower
 // that we have access to the top object of the stack.
 
+// Implement a stack using an array in JavaScript.
+class Stack {
+  List<int> stack = [];
+
+  void push(int data) {
+    stack.add(data);
+  }
+
+  void pop() {
+    if (stack.isEmpty) return null;
+    stack.removeLast();
+  }
+
+  int peek() {
+    if (stack.isEmpty) return -1;
+    return stack.last;
+  }
+
+  bool get isEmpty => stack.isEmpty;
+
+  int get size => stack.length;
+
+  void printStack() {
+    print(stack);
+  }
+}
