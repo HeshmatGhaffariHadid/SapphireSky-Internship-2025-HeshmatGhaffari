b89492bb2cf790e1e15dca9fcab8907312da5433
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-23 22:34:58 +0430

    Stack using one Queue implemented

--- stack/hard.dart
+++ stack/hard.dart
@@ -1,13 +1,4 @@
-void main() {
-  Stack stack = Stack();
-  stack.push(9);
-  stack.push(13);
-  stack.push(1);
-  stack.push(-3);
-  stack.pop();
-  stack.push(-5);
-  print(stack.getMin(stack));
-}
+import 'dart:collection';
 
 // How do you design a stack that supports push(), pop(), and getMin() in constant time?
 class Node<T> {
@@ -63,4 +54,44 @@ class Stack<T> {
     }
     return min.pop()!;
   }
+  // Implement an LRU (Least Recently Used) cache using a stack.
+  // I didn't have any idea in this concept
+}
+
+// How do you implement a stack using only one queue?
+class StackUsingQueue {
+  Queue queue = Queue();
+
+  void push(int value) {
+    queue.add(value);
+    for (int i = 0; i < queue.length -1; i++) {
+      int poped = queue.removeFirst();
+      queue.add(poped);
+    }
+  } // [1,2,3,4,5] add -> 6 ==> [1,2,3,4,5,6] loop -> [6,1,2,3,4,5]
+
+  int? pop() {
+    if (queue.isEmpty) return null;
+    return queue.removeFirst();
+  }
+
+  int top() {
+    return queue.isEmpty ? null : queue.first;
+  }
+
+  bool get isEmpty => queue.isEmpty;
+}
+
+void main() {
+  StackUsingQueue stackUsingQueue = StackUsingQueue();
+  print(stackUsingQueue.isEmpty);
+  stackUsingQueue.push(12);
+  stackUsingQueue.push(13);
+  stackUsingQueue.push(14);
+  stackUsingQueue.push(15);
+  stackUsingQueue.push(10);
+  print(stackUsingQueue.top());
+  stackUsingQueue.pop();
+  print(stackUsingQueue.top());
+  print(stackUsingQueue.isEmpty);
 }
