740f2f7164a4c9cd67e91b66ac4db89446005c3a
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-25 14:57:16 +0430

    Queue defined conceptualy

---
+++ queue/simple.dart
@@ -0,0 +1,6 @@
+// 1- What is a queue, and how does it follow the FIFO principle?
+// answer: Queue is linear data structure that follows FIFO (First In First Out) principle,
+// FIFO principle means that the first element inserted into the queue will the first reserved
+// (processed or popped) from the queue, therefore Queue is like a line of a bakery.
+
+
