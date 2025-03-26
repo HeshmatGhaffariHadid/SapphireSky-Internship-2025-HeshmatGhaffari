1185ceb3974c95582ea459813711693c630fa1fd
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-03-26 14:47:47 +0430

    Queue using linked list implemented

--- queue/simple.dart
+++ queue/simple.dart
@@ -4,6 +4,8 @@
 // (processed or popped) from the queue, therefore Queue is like a line of a bakery.
 
 // 2- Implement a queue using an array in Dart.
+import 'dart:io';
+
 class QueueList<T> {
   List<T> list = [];
 
@@ -35,19 +37,93 @@ class QueueList<T> {
     print('Queue: $list');
   }
 }
+// void main() {
+//   QueueList queue = QueueList();
+//   print('Queue is empty: ${queue.isEmpty}');
+//   queue.enqueue(10);
+//   queue.enqueue(11);
+//   queue.enqueue(12);
+//   queue.display();
+//   print('Queue size: ${queue.size}');
+//   print('Queue front: ${queue.peek()}');
+//   print('Dequeue: ${queue.dequeue()}');
+//   queue.display();
+//   print('Queue size: ${queue.size}');
+//   print('Queue front: ${queue.peek()}');
+//   print('Queue is empty: ${queue.isEmpty}');
+// }
 
-void main() {
-  QueueList queue = QueueList();
-  print('Queue is empty: ${queue.isEmpty}');
-  queue.enqueue(10);
-  queue.enqueue(11);
-  queue.enqueue(12);
-  queue.display();
-  print('Queue size: ${queue.size}');
-  print('Queue front: ${queue.peek()}');
-  print('Dequeue: ${queue.dequeue()}');
-  queue.display();
-  print('Queue size: ${queue.size}');
-  print('Queue front: ${queue.peek()}');
-  print('Queue is empty: ${queue.isEmpty}');
+// How do you implement a queue using a linked list?
+class Node<T> {
+  T data;
+  Node<T>? next;
+  Node(this.data);
 }
+
+class Queue<T> {
+  Node? front;
+  Node? rear;
+  int size = 0;
+
+  bool get isEmpty => front == null;
+
+  void enqueue(T value) {
+    Node? newNode = Node(value);
+    if (isEmpty) {
+      front = rear = newNode;
+    } else {
+      rear!.next = newNode;
+      rear = newNode;
+    }
+    size++;
+  }
+
+  T? dequeue() {
+    if (isEmpty) {
+      print('Queue is empty!');
+      return null;
+    }
+    T data = front!.data;
+    front = front!.next;
+    if (front == null) {
+      rear = null;
+    }
+    size--;
+    return data;
+  }
+
+  T? peek() {
+    if (isEmpty) {
+      print('Queue is empty!');
+      return null;
+    }
+    return front!.data;
+  }
+
+  void display() {
+    if (isEmpty) {
+      print('Queue is empty!');
+    } else {
+      Node? current = front;
+      while (current != null) {
+        print(current.data);
+        current = current.next;
+      }
+    }
+  }
+}
+// void main() {
+//   Queue queue = Queue();
+//   print('Queue is empty: ${queue.isEmpty}');
+//   queue.enqueue(1);
+//   queue.enqueue(2);
+//   queue.enqueue(3);
+//   print('Queue\'s front data: ${queue.front!.data}');
+//   print('Queue\'s rear data: ${queue.rear!.data}');
+//   queue.display();
+//   print('Queue size: ${queue.size}');
+//   queue.dequeue();
+//   queue.display();
+//   print('Queue size: ${queue.size}');
+//   print('Queue is empty: ${queue.isEmpty}');
+// }
