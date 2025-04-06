f0a74c0714af1a6d2e780bb810bec9f5389b4b59
Heshmat Ghaffari <heshmatghaffari123@gmail.com>
2025-04-06 16:41:13 +0430

    circular queue implemented

--- queue/medium.dart
+++ queue/medium.dart
@@ -72,13 +72,75 @@ class NewQueue<T> {
 
 // Aplications of Priority Queues
 // 1- CPU Scheduling:
-// Operating systems use priority queues to manage process execution. Processes with higher 
+// Operating systems use priority queues to manage process execution. Processes with higher
 // priority are given access to the CPU before others.
 // 2- Dijkstra’s and A Algorithms*:
-// In graph algorithms for finding the shortest path, a priority queue helps in selecting the 
+// In graph algorithms for finding the shortest path, a priority queue helps in selecting the
 // next vertex with the shortest distance.
 // 3- Data Compression (Huffman Coding):
 // Used to build a Huffman tree, where characters with the lowest frequency are combined first.
 // 4- Network Routing:
 // Routers use priority queues to determine the order of packet transmission, prioritizing
 // certain types of traffic.
+
+// Implement a circular queue and explain how it works.
+/// CircularQueue class using a generic linked list
+class CircularQueue<T> {
+  Node<T>? front;
+  Node<T>? rear;
+
+  bool get isEmpty => front == null;
+
+  void enqueue(T data) {
+    Node<T>? newNode = Node<T>(data);
+
+    if (isEmpty) {
+      front = rear = newNode;
+      rear!.next = front; //to make it circular
+    } else {
+      rear!.next = newNode;
+      rear = newNode;
+      rear!.next = front; //keep it circular
+    }
+  }
+
+  T? tdequeue() {
+    if (isEmpty) {
+      print("Queue is empty!");
+      return null;
+    }
+    T removedData = front!.data!;
+    if (front == rear) {
+      front = rear = null;
+    } else {
+      front = front!.next;
+      rear!.next = front;
+    }
+    return removedData;
+  }
+
+  T? peek() {
+    if (isEmpty) {
+      print("Queue is empty!");
+      return null;
+    }
+    return front!.data;
+  }
+
+  void display() {
+    if (isEmpty) {
+      print("Queue is empty.");
+      return;
+    }
+
+    print("Queue elements:");
+    Node<T>? current = front;
+    do {
+      print(current!.data);
+      current = current.next;
+    } while (current != front); // it stops when reaches the front again and avoid imfinte loop 
+  }
+}
+// How it works: A circular queue is a data structure that follows the FIFO (First-In-First-Out)
+// principle but with a circular manner, it means that the last position of the queue is attached
+// to the first position when it is full, and elements are dequeued in a cyclic manner.
